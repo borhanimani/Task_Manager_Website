115b704c84bcb19a27421f43e641827e2b17ed35
Borhan <manibor99@gmail.com>
2026-08-09 19:13:29 +0330

    fix structure (django project name)

--- task_manager_website/task_manager_website/asgi.py
+++ task_manager_website/task_manager_website/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for task_manger_website project.
+ASGI config for task_manager_website project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manger_website.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager_website.settings')
 
 application = get_asgi_application()
