ead000b1173c41e3712013310596bf16245783f0
Borhan <manibor99@gmail.com>
2026-08-10 13:51:35 +0330

    make api for task

--- task_manager_website/task/views.py
+++ task_manager_website/task/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
+class TaskView(View):
+    def get(self, request):
+        return render(request, 'task/task_list.html')
--- task_manager_website/task_manager_website/urls.py
+++ task_manager_website/task_manager_website/urls.py
@@ -15,10 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from home.views import HomeView
+from django.contrib.auth import auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
+    path('tasks/', include('task.urls'), name='task_list'),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
+
+try:
+    from . import github_settings
+    social_login = 'authentication/login.html'
+    urlpatterns.insert(0,
+                       path('accounts/login/', auth_views.LoginView.as_view(template_name=social_login))
+                       )
+    print('Using', social_login, 'as the login template')
+except:
+    print('Using registration/login.html as the login template')
