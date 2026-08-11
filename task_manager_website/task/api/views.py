9a004802a6f1bdf49941a01a3a39ce40ae39f15a
Borhan <manibor99@gmail.com>
2026-08-11 19:50:45 +0330

    add search and filter to tasl list view system

--- task_manager_website/task/api/views.py
+++ task_manager_website/task/api/views.py
@@ -3,11 +3,27 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .serializer import TaskSerializer
 from ..models import Task
+from django.db.models import Q
 
 class TaskListAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        tasks = Task.objects.all()
+
+        search = request.query_params.get("search")
+
+        if search:
+            tasks = Task.objects.filter(
+            Q(title__icontains=search) |
+            Q(description__icontains=search))
+        else:
+            tasks = Task.objects.all()
+
+        if request.GET.get('onlyme') == 'true':
+            tasks = tasks.filter(user = request.user)
+            print(tasks)
+
         serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data)
+    
+    
