8fc99d820daf37cb3156c2ead528e31350b7071b
Borhan <manibor99@gmail.com>
2026-08-11 18:29:32 +0330

    fix serializer bugs

--- task_manager_website/task/api/serializer.py
+++ task_manager_website/task/api/serializer.py
@@ -1,14 +1,28 @@
 from rest_framework import serializers
 from ..models import Task
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username"]
+
 
 class TaskSerializer(serializers.ModelSerializer):
-        
-    user = serializers.CharField(
-    source="user.username",
-    read_only=True
-    )
-        
-    class Meta:
 
+    created_by = UserSerializer(
+        source="user",
+        read_only=True
+    )
+
+    class Meta:
         model = Task
-        fields = '__all__'
+        fields = [
+            "id",
+            "title",
+            "description",
+            "status",
+            "created_at",
+            "updated_at",
+            "created_by",
+        ]
